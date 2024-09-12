6e54b630953513507c1fb3fb0351ab7c9a6df21c
FAITH <faithokereke26@gmail.com>
2024-09-12 21:53:02 +0100

    changes

--- src/Components/Feautures.jsx
+++ src/Components/Feautures.jsx
@@ -10,21 +10,21 @@ export default function Feautures() {
       heading: "Bookmark in one click",
       description:
         "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
-      image: firstTabImage,
+      image: "src/assets/illustration-features-tab-1.svg",
     },
     {
       title: "Speedy Searching",
       heading: "Intelligent search",
       description:
         "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
-      image: secondTabImage,
+      image: "src/assets/illustration-features-tab-2.svg",
     },
     {
       title: "Easy Sharing",
       heading: "Share your bookmarks",
       description:
         "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
-      image: thirdTabImage,
+      image: "src/assets/illustration-features-tab-3.svg",
     },
   ];
   return (
